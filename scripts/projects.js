8e2cd7a0a7810e35ca022aad954a06c3ef7d3c6d
David Livingstone <david@david.tl>
2016-01-05 21:47:40 -0800

    added raw project data and social media links

---
+++ scripts/projects.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var rawData = [
+  {
+    title:        'Can I Recycle?',
+    url:          'http://www.trishthedish.github.io',
+    screenshot:   'img/recycle.png',
+    description:  'Can I Recycle can help more accurately recycle, compost, and throw away waste in the city of Seattle. Its simple navigation helps you find out how to dispose of an item fast, and resources direct you to organizations that take donated good.'
+  },
+  {
+    title:        'Digital Business Card',
+    url:          'index.html',
+    screenshot:   'img/card.png',
+    description:  'I build this portfolio to serve as a digital business card to showcase projects and provide contact informtion.'
+  }
+];
