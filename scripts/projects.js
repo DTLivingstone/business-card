beb21060eb9781a25b3cb22180b7a786935db848
David Livingstone <david@david.tl>
2016-01-14 15:06:32 -0800

    started adding skeletons of Projects and projectData methods

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var rawProjects = [
+var projectData = [
   {
     title:        'Can I Recycle?',
     url:          'http://www.trishthedish.github.io',
     screenshot:   'img/recycle.png',
     description:  'Can I Recycle can help more accurately recycle, compost, and throw away waste in the city of Seattle. Its simple navigation helps you find out how to dispose of an item fast, and resources direct you to organizations that take donated good.',
-    date: '2015/12/28'
+    pubDate: '2015-12-28'
   },
   {
     title:        'Digital Business Card',
     url:          'index.html',
     screenshot:   'img/card.png',
     description:  'I build this portfolio to serve as a digital business card to showcase projects and provide contact informtion.',
-    date: '2015/1/4'
+    pubDate: '2015-1-4'
   }
 ];
--- scripts/projectsRender.js
+++ scripts/projectsRender.js
@@ -2,4 +2,26 @@
 
 var projects = [];
 
-var Projects = function {
+var Projects = function(obj) {
+this.title = obj.title;
+this.url = obj.url;
+this.screenshot = obj.screenshot;
+this.description = obj.description;
+this.date = obj.date;
+};
+
+var Projects.prototype.builtHtml = function() {
+  //render HTML
+  return '<span>hi </span>';
+};
+
+projectData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+};
+
+projectData.forEach(function(obj) {
+  projects.push(new Project(obj));
+};
+
+projects.forEach(function(obj) {
+  $('#portfolio').append(?.buildHtml)
