d101fa914e76359753d731ef66b16e0f5ba9b35d
David Livingstone <david@david.tl>
2016-01-14 17:16:27 -0800

    linted

--- scripts/projectsRender.js
+++ scripts/projectsRender.js
@@ -1,27 +1,28 @@
 'use strict';
 
-var projects = [];
+var allProjects = [];
 
-var Projects = function(obj) {
-this.title = obj.title;
-this.url = obj.url;
-this.screenshot = obj.screenshot;
-this.description = obj.description;
-this.date = obj.date;
+var Project = function(obj) {
+  this.title = obj.title;
+  this.url = obj.url;
+  this.screenshot = obj.screenshot;
+  this.description = obj.description;
+  this.date = obj.date;
 };
 
-var Projects.prototype.builtHtml = function() {
+Project.prototype.buildHtml = function() {
   //render HTML
   return '<span>hi </span>';
 };
 
 projectData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-};
+});
 
 projectData.forEach(function(obj) {
   projects.push(new Project(obj));
-};
+});
 
 projects.forEach(function(obj) {
-  $('#portfolio').append(?.buildHtml)
+  $('#portfolio').append(todo.buildHtml);
+});
