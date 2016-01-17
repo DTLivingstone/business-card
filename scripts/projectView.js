2875b9010dca91965f3d1bd78d74389bc45e0e85
David Livingstone <david@david.tl>
2016-01-16 16:16:31 -0800

    filters, about tab, and main nav are fully functioning, template is hidden

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -2,13 +2,25 @@
 
 var projectView = {};
 
+projectView.handleNav = function() {
+  $('nav').on('click', '.tab', function(xxx) {
+    $('.tab-content').hide();
+    var foo = $(this).data('content');
+    $('#' + $(this).data('content')).fadeIn(100);
+  });
+
+  $('nav .tab:first').click();
+};
+
 projectView.populateFilters = function() {
   $('article').each(function() {
     if (!$(this).hasClass('template')) {
       var category = $(this).attr('data-category');
-      $('#category-filter').append('<option value="' + category + '">' + category + '</option>');
+      if ($('#category-filter option[value ="' + category + '"]').length === 0) {
+        $('#category-filter').append('<option value="' + category + '">' + category + '</option>');
+      }
       var year = $(this).attr('data-year');
-      console.log($('#year-filter option[value="' + year + '"]').length === 0);
+      // console.log($('#year-filter option[value="' + year + '"]').length === 0);
       if (($('#year-filter option[value = "' + year + '"]').length === 0)) {
         $('#year-filter').append('<option value="' + year + '">' + year + '</option>');
       }
@@ -17,5 +29,6 @@ projectView.populateFilters = function() {
 };
 
 $(function() {
+  projectView.handleNav();
   projectView.populateFilters();
 });
