f43bde212d25b8c20efbc63001f87dbb30619e24
David Livingstone <david@david.tl>
2016-02-04 09:16:06 -0800

    template rendering and filters working again

--- scripts/projectModel.js
+++ scripts/projectModel.js
@@ -9,20 +9,12 @@ var Project = function(obj) {
   this.screenshot = obj.screenshot;
   this.description = obj.description;
   this.pubDate = obj.pubDate;
+  this.year = obj.pubDate.slice(0,4);
 };
 
 Project.prototype.buildHtml = function() {
-  var $newProject = $('article.template').clone();
-  $newProject.removeClass('template');
-  $newProject.attr('data-category', this.category);
-  $newProject.attr('data-year', this.pubDate.slice(0,4));
-  $newProject.find('a').attr('href', this.url);
-  $newProject.find('a h2').html(this.title);
-  $newProject.find('.category').html(this.category);
-  // pubdate not displayed yet
-  $newProject.find('img').attr('src', this.screenshot);
-  $newProject.find('p').html(this.description);
-  return $newProject;
+  var template = Handlebars.compile($('#article-template').html());
+  return template(this);
 };
 
 projectData.sort(function(a,b) {
@@ -33,7 +25,6 @@ projectData.forEach(function(obj) {
   allProjects.push(new Project(obj));
 });
 
-// not sure where this belongs in the MVC model
 allProjects.forEach(function(obj) {
   $('#portfolio').append(obj.buildHtml());
 });
