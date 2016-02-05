dbf15120bf0e04cfb9639a06f0fdfe9b21958af1
David Livingstone <david@david.tl>
2016-02-04 22:54:55 -0800

    wrapped in IIFEs

--- scripts/projectModel.js
+++ scripts/projectModel.js
@@ -1,61 +1,65 @@
 'use strict';
 
-var Project = function(obj) {
-  this.title = obj.title;
-  this.category = obj.category;
-  this.url = obj.url;
-  this.screenshot = obj.screenshot;
-  this.description = obj.description;
-  this.pubDate = obj.pubDate;
-  this.year = obj.pubDate.slice(0,4);
-};
+(function(module) {
 
-Project.all = [];
+  var Project = function(obj) {
+    this.title = obj.title;
+    this.category = obj.category;
+    this.url = obj.url;
+    this.screenshot = obj.screenshot;
+    this.description = obj.description;
+    this.pubDate = obj.pubDate;
+    this.year = obj.pubDate.slice(0,4);
+  };
 
-Project.prototype.buildHtml = function() {
-  var template = Handlebars.compile($('#article-template').html());
-  return template(this);
-};
+  Project.all = [];
 
-Project.loadAll = function(data) {
-  data.sort(function(a,b) {
-    return (new Date(b.pubDate)) - (new Date(a.pubDate));
-  });
-  data.forEach(function(obj) {
-    Project.all.push(new Project(obj));
-  });
-};
+  Project.prototype.buildHtml = function() {
+    var template = Handlebars.compile($('#article-template').html());
+    return template(this);
+  };
 
-Project.fetchAll = function() {
-  if (localStorage.data) {
+  Project.loadAll = function(data) {
+    data.sort(function(a,b) {
+      return (new Date(b.pubDate)) - (new Date(a.pubDate));
+    });
+    data.forEach(function(obj) {
+      Project.all.push(new Project(obj));
+    });
+  };
+
+  Project.fetchAll = function() {
+    if (localStorage.data) {
+      $.ajax({
+        url: 'data/projects.json',
+        type: 'HEAD',
+        success: function (data, message, xhr) {
+          var getETag = xhr.getResponseHeader('ETag');
+          if(getETag === JSON.parse(localStorage.savedETag)) {
+            Project.loadAll(JSON.parse(localStorage.data));
+            projectView.initIndex();
+          } else {
+            Project.serverGrab();
+          }
+        }
+      });
+    } else {
+      Project.serverGrab();
+    }
+  };
+
+  Project.serverGrab = function() {
     $.ajax({
       url: 'data/projects.json',
-      type: 'HEAD',
-      success: function (data, message, xhr) {
-        var getETag = xhr.getResponseHeader('ETag');
-        if(getETag === JSON.parse(localStorage.savedETag)) {
-          Project.loadAll(JSON.parse(localStorage.data));
-          projectView.initIndex();
-        } else {
-          Project.serverGrab();
-        }
+      type: 'GET',
+      dataType: 'JSON',
+      success: function(rawData, message, xhr) {
+        Project.loadAll(rawData);
+        localStorage.data = JSON.stringify(rawData);
+        projectView.initIndex();
+        localStorage.savedETag = JSON.stringify(xhr.getResponseHeader('ETag'));
       }
     });
-  } else {
-    Project.serverGrab();
-  }
-};
-
-Project.serverGrab = function() {
-  $.ajax({
-    url: 'data/projects.json',
-    type: 'GET',
-    dataType: 'JSON',
-    success: function(rawData, message, xhr) {
-      Project.loadAll(rawData);
-      localStorage.data = JSON.stringify(rawData);
-      projectView.initIndex();
-      localStorage.savedETag = JSON.stringify(xhr.getResponseHeader('ETag'));
-    }
-  });
-};
+  };
+  module.Project = Project;
+})(window);
--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,81 +1,86 @@
 'use strict';
 
-var projectView = {};
+(function(module) {
 
-projectView.handleMenu = function() {
-  $('#hamburger').on('click', function() {
-    $('nav').toggle();
-  });
-};
+  var projectView = {};
 
-projectView.handleResize = function() {
-  $(window).resize(function() {
-    if ($('#hamburger').css('display') === 'block') {
-      $('nav').hide();
-    } else {
-      $('nav').show();
-    }
-  });
-};
+  projectView.handleMenu = function() {
+    $('#hamburger').on('click', function() {
+      $('nav').toggle();
+    });
+  };
 
-projectView.handleNav = function() {
-  $('nav').on('click', '.tab', function(a) {
-    $('.tab-content').hide();
-    var foo = $(this).data('content');
-    $('#' + $(this).data('content')).fadeIn(150);
-  });
-  $('nav .tab:first').click();
-};
-
-projectView.populateFilters = function() {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var category = $(this).attr('data-category');
-      if ($('#category-filter option[value ="' + category + '"]').length === 0) {
-        $('#category-filter').append('<option value="' + category + '">' + category + '</option>');
+  projectView.handleResize = function() {
+    $(window).resize(function() {
+      if ($('#hamburger').css('display') === 'block') {
+        $('nav').hide();
+      } else {
+        $('nav').show();
       }
-      var year = $(this).attr('data-year');
-      if (($('#year-filter option[value = "' + year + '"]').length === 0)) {
-        $('#year-filter').append('<option value="' + year + '">' + year + '</option>');
+    });
+  };
+
+  projectView.handleNav = function() {
+    $('nav').on('click', '.tab', function(a) {
+      $('.tab-content').hide();
+      var foo = $(this).data('content');
+      $('#' + $(this).data('content')).fadeIn(150);
+    });
+    $('nav .tab:first').click();
+  };
+
+  projectView.populateFilters = function() {
+    $('article').each(function() {
+      if (!$(this).hasClass('template')) {
+        var category = $(this).attr('data-category');
+        if ($('#category-filter option[value ="' + category + '"]').length === 0) {
+          $('#category-filter').append('<option value="' + category + '">' + category + '</option>');
+        }
+        var year = $(this).attr('data-year');
+        if (($('#year-filter option[value = "' + year + '"]').length === 0)) {
+          $('#year-filter').append('<option value="' + year + '">' + year + '</option>');
+        }
       }
-    }
-  });
-};
+    });
+  };
 
-projectView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-category="' + $(this).val() + '"]').fadeIn(150);
-    } else {
-      $('article').fadeIn(150);
-      $('article.template').hide();
-    }
-    $('#year-filter').val('');
-  });
-};
+  projectView.handleCategoryFilter = function() {
+    $('#category-filter').on('change', function() {
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-category="' + $(this).val() + '"]').fadeIn(150);
+      } else {
+        $('article').fadeIn(150);
+        $('article.template').hide();
+      }
+      $('#year-filter').val('');
+    });
+  };
 
-projectView.handleYearFilter = function() {
-  $('#year-filter').on('change', function() {
-    if ($(this).val()) {
-      $('article').hide();
-      $('article[data-year="' + $(this).val() + '"]').fadeIn(150);
-    } else {
-      $('article').fadeIn(150);
-      $('article.template').hide();
-    }
-    $('#category-filter').val('');
-  });
-};
+  projectView.handleYearFilter = function() {
+    $('#year-filter').on('change', function() {
+      if ($(this).val()) {
+        $('article').hide();
+        $('article[data-year="' + $(this).val() + '"]').fadeIn(150);
+      } else {
+        $('article').fadeIn(150);
+        $('article.template').hide();
+      }
+      $('#category-filter').val('');
+    });
+  };
 
-projectView.initIndex = function() {
-  Project.all.forEach(function(obj) {
-    $('#portfolio').append(obj.buildHtml());
-  });
-  projectView.handleNav();
-  projectView.handleResize();
-  projectView.populateFilters();
-  projectView.handleCategoryFilter();
-  projectView.handleYearFilter();
-  projectView.handleMenu();
-};
+  projectView.initIndex = function() {
+    Project.all.forEach(function(obj) {
+      $('#portfolio').append(obj.buildHtml());
+    });
+    projectView.handleNav();
+    projectView.handleResize();
+    projectView.populateFilters();
+    projectView.handleCategoryFilter();
+    projectView.handleYearFilter();
+    projectView.handleMenu();
+  };
+
+  module.projectView = projectView;
+})(window);
